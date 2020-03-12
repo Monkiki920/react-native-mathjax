660795ede48655d353e3cbe5356c9451bafa5d6b
Monkiki920 <1049260505@qq.com>
2020-03-12 15:15:03 +0800

    兼容最新react-native版本

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
 import React from 'react';
-import { View, WebView } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const defaultOptions = {
-	messageStyle: 'none',
-	extensions: ['tex2jax.js'],
-	jax: ['input/TeX', 'output/HTML-CSS'],
-	tex2jax: {
-		inlineMath: [['$', '$'], ['\\(', '\\)']],
-		displayMath: [['$$', '$$'], ['\\[', '\\]']],
-		processEscapes: true,
-	},
-	TeX: {
-		extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
-	}
+    messageStyle: 'none',
+    extensions: ['tex2jax.js'],
+    jax: ['input/TeX', 'output/HTML-CSS'],
+    tex2jax: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true,
+    },
+    TeX: {
+        extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
+    }
 };
 
 class MathJax extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			height: 1
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: 100
+        };
+    }
 
-	handleMessage(message) {
-		this.setState({
-			height: Number(message.nativeEvent.data)
-		});
-	}
+    handleMessage(message) {
+        this.setState({
+            height: Number(message.nativeEvent.data)
+        });
+    }
 
-	wrapMathjax(content) {
-		const options = JSON.stringify(
-			Object.assign({}, defaultOptions, this.props.mathJaxOptions)
-		);
+    wrapMathjax(content) {
+        const options = JSON.stringify(
+            Object.assign({}, defaultOptions, this.props.mathJaxOptions)
+        );
 
-		return `
+        return `
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 			<script type="text/x-mathjax-config">
 				MathJax.Hub.Config(${options});
 
 				MathJax.Hub.Queue(function() {
 					var height = document.documentElement.scrollHeight;
+
 					window.postMessage(String(height));
-					document.getElementById("formula").style.visibility = '';
 				});
 			</script>
 
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
-			<div id="formula" style="visibility: hidden;">
-				${content}
-			</div>
+			
+			<script type="text/javascript" async
+			src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML">
+		  </script>
+			${content}
+			
 		`;
-	}
-	render() {
-		const html = this.wrapMathjax(this.props.html);
+    }
+    //<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
 
-		// Create new props without `props.html` field. Since it's deprecated.
-		const props = Object.assign({}, this.props, { html: undefined });
+    render() {
+        const html = this.wrapMathjax(this.props.html);
 
-		return (
-			<View style={{ height: this.state.height, ...props.style }}>
-				<WebView
-					scrollEnabled={false}
-					onMessage={this.handleMessage.bind(this)}
-					source={{ html }}
-					{...props}
-				/>
-			</View>
-		);
-	}
+        // Create new props without `props.html` field. Since it's deprecated.
+        const props = Object.assign({}, this.props, { html: undefined });
+        return <WebView
+            originWhitelist={['*']}
+            scrollEnabled={true}
+            onMessage={this.handleMessage.bind(this)}
+            source={{ html }}
+            {...props}
+        />
+    }
 }
 
 export default MathJax;
